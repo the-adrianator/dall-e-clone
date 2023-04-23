7f6d99b59b633794e86e3b24237e5640f8cc37ff
CodeGenie.dev <adrian@codegenie.dev>
2023-04-23 09:43:12 +0100

    set up post and dall-e routes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './mongoDB/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dallERoutes from './routes/dallERoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dallE', dallERoutes)
 
 app.get('/', async (req, res) => {
   res.send('Hello from DALL-E!')
